02c8db36e042e7e1274f568a3b75922ddfcc9a10
Patricio <pcifrey501@g.educaand.es>
2024-02-08 14:23:59 +0100

    work on: styles of about/login pages

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -18,18 +18,31 @@ export default {
 </script>
 
 <template>
-  <section class="temp">
-    <h1>Login</h1>
-    <button @click="auth">Enter</button>
-    <router-link to="/register">Register</router-link>
+  <section class="login">
+
+    <article class="login__header">
+      <img alt="point" class="header__decoration" src="../../public/pointpoint.svg">
+      <h1>Login</h1>
+      <img alt="point" class="header__decoration" src="../../public/pointpoint.svg">
+    </article>
+
+    <form class="login__form">
+      <input placeholder="Username..."/>
+      <input type="password" placeholder="Password..."/>
+    </form>
+
+    <button @click="auth" class="primary-button">Enter</button>
+
+    <article class="login__register-anchor">
+      <p>Don't have an account? </p>
+      <router-link to="/register">Register</router-link>
+      <article class="register-anchor__free">
+        <router-link to="/register" class="free__link">free</router-link>
+        <img  class="login__doodle" alt="hearts decoration" src="../../public/hearts.svg">
+      </article>
+      </article>
+
+
   </section>
 </template>
 
-<style scoped>
-  .temp{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
-</style>
