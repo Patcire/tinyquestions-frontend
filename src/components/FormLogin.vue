bc1423b92e1228044e59ee3b92b10cff430b20fe
Patricio <pcifrey501@g.educaand.es>
2024-02-14 08:48:08 +0100

    prevents submit the form if is not validated correctly

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -2,7 +2,7 @@
 import { useSessionStore } from '@/stores/sessionStore.js'
 import router from '@/router/router.js'
 import { validationSchema } from '@/helpers/formHelpers.js'
-import { ErrorMessage, Field, Form } from 'vee-validate'
+import { ErrorMessage, Field, Form} from 'vee-validate'
 
 export default {
   name: 'FormLogin',
@@ -13,11 +13,10 @@ export default {
     }
   },
   methods: {
-   handleLoguin(e) {
-     //e.preventDefault();
-    this.sessionStore.checkUserCredentials()
-     router.push('/games')
-     console.log('pasa')
+   handleLoguin() {
+     this.sessionStore.checkUserCredentials()
+     //router.push('/games')
+         router.push('/games')
     }
   },
   components:{
@@ -29,11 +28,12 @@ export default {
 </script>
 
 <template>
-  <vForm class="form" :validation-schema="this.schema">
+
+  <vForm class="form" :validation-schema="this.schema" v-slot="{submitForm}">
     <vField placeholder="Username..." class="form__input" type="string" name="username"/>
     <vError name="username" class="contact__error"></vError>
     <vField type="password" name="password" placeholder="Password..." class="form__input"/>
     <vError name="password" class="contact__error"></vError>
-    <button type="submit" @click="handleLoguin" class="primary-button">Enter</button>
+    <button type="submit" @click="submitForm(handleLoguin)" class="primary-button">Enter</button>
   </vForm>
 </template>
