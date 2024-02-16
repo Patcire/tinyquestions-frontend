2a8b6413a79e66a2333226d459d2e1edd2f5f86f
Patricio <pcifrey501@g.educaand.es>
2024-02-16 13:40:46 +0100

    delete: unnecessary provider deleted

--- src/main.js
+++ src/main.js
@@ -3,14 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/router.js'
 import { createPinia } from 'pinia'
-import { useSessionStore } from '@/stores/sessionStore.js'
-
 
 const app = createApp(App)
 const pinia = createPinia()
-app.use(pinia)
-
-const seshStore = useSessionStore()
-app.provide('seshStore', seshStore)
-
-app.use(router).mount('#app')
+app.use(pinia).use(router).mount('#app')
