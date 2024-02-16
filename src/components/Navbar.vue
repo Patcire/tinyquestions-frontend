9c9a275f80cba37cfaedc04266079c3637f2e182
Patricio <pcifrey501@g.educaand.es>
2024-02-16 11:37:42 +0100

    implemented: hamburger menÃu in mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -20,7 +20,7 @@ export default {
 
 <template>
 
-  <section>
+  <nav>
 
     <ul v-if="!this.sessionStore.isConnected" class="navbar">
       <li>
@@ -63,6 +63,63 @@ export default {
         <a @click="auth" class="navbar__link pointer" >Exit</a>
       </li>
     </ul>
-  </section>
+  </nav>
+
+  <!--For mobile-->
+  <nav class= "hambmenu" v-if="!this.sessionStore.isConnected">
+    <article class="hambmenu__icon">
+      <input type="checkbox" class="hambmenu__check">
+      <label for="hambmenu__check">
+        <img src="../../public/hambmenu.svg"
+             alt="Menu icon"
+              class="hambmenu__img">
+      </label>
+      <ul class="hambmenu__submenu">
+        <li>
+          <router-link to="/"  class="navbar__link">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/play" class="navbar__link">Play</router-link>
+        </li>
+        <li>
+          <router-link to="/about" class="navbar__link">About</router-link>
+        </li>
+      </ul>
+    </article>
+  </nav>
+
+  <nav class= "hambmenu" v-if="this.sessionStore.isConnected">
+    <article class="hambmenu__icon">
+      <input type="checkbox" class="hambmenu__check">
+      <label for="hambmenu__check">
+        <img src="../../public/hambmenu.svg"
+             alt="Menu icon"
+             class="hambmenu__img">
+      </label>
+      <ul class="hambmenu__submenu">
+        <li>
+          <router-link to="/games" class="navbar__link">Games</router-link>
+        </li>
+        <li>
+          <router-link to="/create" class="navbar__link" >Create</router-link>
+        </li>
+        <li class="navbar__submenu">
+          <button class="navbar__button">Comunnity</button>
+        </li>
+        <li>
+          <router-link to="/profile" class="navbar__sublink" >My profile</router-link>
+        </li>
+        <li>
+          <router-link to="/find" class="navbar__sublink" >Find questions</router-link>
+        </li>
+        <li>
+          <router-link to="/ranks" class="navbar__sublink">Ranks</router-link>
+        </li>
+        <li>
+          <a @click="auth" class="navbar__link pointer" >Exit</a>
+        </li>
+      </ul>
+    </article>
+  </nav>
 
 </template>
