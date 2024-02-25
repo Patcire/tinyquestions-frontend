dc836899b3725ad6036749f26cc1bffd676ed11d
Patricio <pcifrey501@g.educaand.es>
2024-02-25 18:58:23 +0100

    added: responsive of doodles on create

--- src/pages/Create.vue
+++ src/pages/Create.vue
@@ -9,7 +9,19 @@ export default {
 
 <template>
   <section class="create">
+    <div class="create__container-wmelon">
+      <img
+        class="create__wmelon"
+        alt="watermelon doodle"
+        src="../../public/Vectorwmelon.svg">
+    </div>
     <Customizer></Customizer>
+    <div class="create__container-electron">
+      <img
+      class="create__electron"
+      alt="electron doodle"
+      src="../../public/Vectorelectron.svg">
+    </div>
   </section>
 
 </template>
