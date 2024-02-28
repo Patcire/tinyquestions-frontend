c5c7ccc567effe20d0d96116940edee5edfc4a5e
Patricio <pcifrey501@g.educaand.es>
2024-02-28 13:02:49 +0100

    improved: like/dislikes dynamics

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -23,18 +23,32 @@ export default {
     }
   },
   methods:{
-    async giveLike(){
+    async giveLikeOrDislike(){
+      // I aproeached this with the
+      // "optimistic" promise priciples
+      //  i  shopw the like/dislike visually
       this.liked= !this.liked
       if (this.liked===true){
         const likeSaveOnDB = await postAPI(`http://localhost:8000/api/li/give`, {
           "fk_id_user": this.userID,
           "fk_id_quiz": this.quiz.id_quiz
         })
+        // and then, if the API post fail
+        // I undo the visual icon
+        if (!likeSaveOnDB) {
+          this.liked = false
+        }
+        // finally, if all was success, update the LS
+        useSessionStore().user.likedStorage.push(this.quiz)
         return
       }
       console.log('meh, i dislike it really!')
       const likeDeleteOnDB = await deleteAPI(`http://localhost:8000/api/li/dis/${this.userID}/${this.quiz.id_quiz}`)
+      if (!likeDeleteOnDB)  this.liked = true
+      const updateStorage = useSessionStore().user.likedStorage.filter((quizLS) => quizLS.id_quiz !== this.quiz.id_quiz )
+      useSessionStore().user.likedStorage = [...updateStorage]
     }
+
   },
   created() {
     // when we show the quiz we must know if the quizz is already liked
@@ -58,7 +72,7 @@ export default {
     <button class="card__like">
 
       <img
-        @click="giveLike"
+        @click="giveLikeOrDislike"
         class="card__empty-heart"
         alt="doodle of a heart"
         :src="liked ?
