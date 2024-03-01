638282e5b36806414fc84b12df823fc8ea362f75
Patricio <pcifrey501@g.educaand.es>
2024-03-01 12:09:34 +0100

    added: 404 page with aniamtions

--- src/pages/NotFound404.vue
+++ src/pages/NotFound404.vue
@@ -5,7 +5,14 @@ export default {
 </script>
 
 <template>
-  <h1>404</h1>
+  <section class="notFound">
+    <article class="notFound__message">
+      <h1 class="notFound__ouch">OUCH,</h1>
+      <h1 class="notFound__info">PAGE NOT FOUND</h1>
+      <h5 class="notFound__subtext">also known as 404 error</h5>
+    </article>
+    <img class="notFound__img" alt="doodle of a glass and a digital screen" src="../../public/Vector404.svg">
+  </section>
 
 </template>
 
