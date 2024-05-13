441b0cae14d60ac0725cfd132249eeff87fe5e0b
Patricio <pcifrey501@g.educaand.es>
2024-05-13 22:22:36 +0200

    records almost finished

--- src/pages/Records.vue
+++ src/pages/Records.vue
@@ -10,9 +10,11 @@ export default {
     return{
       historic: [],
       pageNumber: 1,
-      selectedIndex: null,
+      selectedMatch: null,
+      selectedQuestion: undefined,
       questionsInfo: {},
-      rightAnswers: 0
+      rightAnswers: 0,
+      answersInfo: []
     }
   },
   methods:{
@@ -23,30 +25,40 @@ export default {
     },
     useSessionStore,
 
-    async handleSelectedItem(index, e){
-      this.selectedIndex = index
+    async handleSelectedItem(index, id){
+      this.selectedMatch = index
       this.historic[index].customQuiz ?
-        this.questionsInfo = await callAPI(`http://localhost:8000/api/ques/allFrom/${e.target.id}`)
+        this.questionsInfo = await callAPI(`http://localhost:8000/api/ques/allFrom/${id}`)
           :
-        this.questionsInfo = await callAPI(`http://localhost:8000/api/has/${e.target.id}`)
+        this.questionsInfo = await callAPI(`http://localhost:8000/api/has/${id}`)
 
+    },
+
+    handleAnswerView(index){
+      // if it is the same but user clicks again, answers will hide
+      this.selectedQuestion === index? this.selectedQuestion = -1 : this.selectedQuestion = index
+      this.answersInfo = JSON.parse(this.historic[this.selectedMatch].answers)
     }
 
   },
 
   async created() {
     let response = await callAPI(`http://localhost:8000/api/play/${useSessionStore().user.userID}/?page=${this.pageNumber}`)
-    this.historic = [...response.data]
+    this.historic = response.data
     console.log(this.historic)
   },
 
   watch:{
 
-    selectedIndex(){
+    selectedMatch(){
       this.rightAnswers = 0
-      JSON.parse(this.historic[this.selectedIndex].answers).forEach((response)=>{
+      JSON.parse(this.historic[this.selectedMatch].answers).forEach((response)=>{
        response.wasRight && this.rightAnswers++
       })
+    },
+
+    questionsInfo(){
+      console.log(this.questionsInfo)
     }
 
   }
@@ -80,8 +92,9 @@ export default {
           class="records__row"
           :id="report.randomQuiz ? report.randomQuiz.id_quiz : report.customQuiz.id_quiz"
           key="index"
-          :class="{selected: selectedIndex === index}"
-          @click="handleSelectedItem(index, $event)"
+          :class="{selected: selectedMatch === index}"
+          @click="handleSelectedItem(index,
+          report.randomQuiz ? report.randomQuiz.id_quiz : report.customQuiz.id_quiz)"
         >
           <p class="records__title" v-if="report.randomQuiz">{{report.randomQuiz.mode}}</p>
           <p v-if="report.randomQuiz"><strong>{{formatDate(report.date)}}</strong></p>
@@ -91,17 +104,15 @@ export default {
 
       </li>
     </ul>
-
   </aside>
 
   <section class="records__inform">
 
-    <article class="records__report"
-      aria-label="Game report"
-    >
+    <article class="records__report" aria-label="Game report">
       <h1 >Game rep</h1>
       <img class="records__glass"
         src="/public/glassglass.svg" alt="glass">
+
       <div class="records__letters">
         <h1 class="records__rest">rt</h1>
       </div>
@@ -114,30 +125,51 @@ export default {
 
 
 
-    <article class="records__quiz" v-if="selectedIndex !== null">
-      <article class="records__summary">
+    <section class="records__quiz" v-if="selectedMatch !== null">
 
+      <article class="records__summary">
         <p class="records__minititle">
           Quiz name/mode:
-          {{(this.historic[selectedIndex].randomQuiz
-            && this.historic[selectedIndex].randomQuiz.mode)
-          ||
-          (this.historic[selectedIndex].customQuiz
-            && this.historic[selectedIndex].customQuiz.quiz_name) }}
+          {{ (this.historic[selectedMatch].randomQuiz
+          && this.historic[selectedMatch].randomQuiz.mode)
+        ||
+        (this.historic[selectedMatch].customQuiz
+          && this.historic[selectedMatch].customQuiz.quiz_name) }}
         </p>
-        <p>Total questions: {{this.historic[selectedIndex].match.quiz.number_questions}}</p>
+        <p>Total questions: {{ this.historic[selectedMatch].match.quiz.number_questions }}</p>
 
         <div class="records__container--relative">
           <p>Total correct answers: {{this.rightAnswers}} </p>
           <img class="records__mini-icon" alt="heart doodle" src="/public/like.svg" >
         </div>
         <div class="records__container--relative">
-          <p>Total fails:  {{this.historic[selectedIndex].match.quiz.number_questions-this.rightAnswers}}</p>
+          <p>Total fails: {{ this.historic[selectedMatch].match.quiz.number_questions - this.rightAnswers }}</p>
           <img class="records__mini-icon" alt="dislike doodle" src="/public/fail.svg" >
         </div>
+      </article>
+
+      <article class="records__questions">
+        <ul>
+          <li v-for="(question, index) in questionsInfo" @click="handleAnswerView(index)">
+
+            <div class="records__container--relgap">
+              <img v-if="selectedQuestion === index" alt="point" src="/public/pointpoint.svg" height="15px">
+              <p class="records__question" :class="{selectedQuestion: selectedQuestion === index}">
+                {{index+1}}. {{question.title}}
+              </p>
+            </div>
+
+            <article class="records__answers" v-if="selectedQuestion === index">
+              <p class="records__yr-answer">Your answer: {{answersInfo[index].selected}}</p>
+              <p>Correct answer: <span class="records__co-answer">{{answersInfo[index].correct}}</span></p>
+            </article>
+
+          </li>
+        </ul>
 
       </article>
-    </article>
+
+    </section>
 
 
   </section>
