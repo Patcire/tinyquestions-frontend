7c17cb2d446387da46e30268f8d076ad80fc80a7
Patricio <pcifrey501@g.educaand.es>
2024-06-05 22:10:17 +0200

    functiolities on records almost finishes

--- src/pages/Records.vue
+++ src/pages/Records.vue
@@ -16,9 +16,9 @@ export default {
       lastPage: 0,
       selectedMatch: null,
       selectedQuestion: undefined,
-      questionsInfo: {},
-      rightAnswers: 0,
-      answersInfo: []
+      questionsInfo: {}, // all questions info (options, title etc.)
+      answersInfo: [], // all user Responses to selected quiz questions (was right, selected etc.)
+      rightAnswers: 0
     }
   },
   methods:{
@@ -29,25 +29,44 @@ export default {
     },
     useSessionStore,
 
-    async handleSelectedItem(index, id){
+    async handleSelectedMatch(index, id){
+      this.answersInfo && console.log(this.answersInfo)
+      console.log('index:' ,index, 'id: ', id)
       this.selectedMatch = index
+      this.rightAnswers = 0
+      this.answersInfo = []
       this.historic[index].customQuiz ?
         this.questionsInfo = await callAPI(`http://localhost:8000/api/ques/allFrom/${id}`)
           :
         this.questionsInfo = await callAPI(`http://localhost:8000/api/has/${id}`)
 
+      if (this.historic[this.selectedMatch].answers) this.answersInfo = JSON.parse(this.historic[this.selectedMatch].answers)
+
+      await this.historic[this.selectedMatch].answers && JSON.parse(this.historic[this.selectedMatch].answers).forEach((response)=>{
+        console.log(response+response.wasRight)
+        response.wasRight && this.rightAnswers++
+      })
     },
 
     handleAnswerView(index){
       // if it is the same but user clicks again, answer will hide
       this.selectedQuestion === index? this.selectedQuestion = -1 : this.selectedQuestion = index
-      this.answersInfo = JSON.parse(this.historic[this.selectedMatch].answers)
     },
 
     async handleCallAPI(page){
       let response = await callAPI(`http://localhost:8000/api/play/${useSessionStore().user.userID}/3/?page=${page}`)
       this.historic = response.data
       this.lastPage = response.last_page
+    },
+
+    handleChangePage(page){
+      if (this.actualPage !== page){
+        this.actualPage = page
+        this.historic = []
+        this.selectedMatch = null
+        this.handleCallAPI(page)
+      }
+
     }
 
   },
@@ -55,27 +74,6 @@ export default {
   async created() {
     await this.handleCallAPI(this.actualPage)
   },
-
-  watch:{
-
-    selectedMatch(){
-      this.rightAnswers = 0
-      this.historic && JSON.parse(this.historic[this.selectedMatch].answers).forEach((response)=>{
-       response.wasRight && this.rightAnswers++
-      })
-      this.answersInfo = JSON.parse(this.historic[this.selectedMatch].answers)
-    },
-
-    questionsInfo(){
-      console.log(this.questionsInfo)
-    },
-
-    actualPage(value){
-      console.log(value)
-      this.selectedMatch = -1
-    }
-
-  }
 
 }
 </script>
@@ -94,9 +92,10 @@ export default {
 
     </header>
 
+    <!-- Matches List-->
     <article v-if="historic.length" class="records__list-container">
       <pagination :actualPage="this.actualPage" :lastPage="this.lastPage"
-        @moveToActualPag="handleCallAPI">
+        @moveToActualPag="handleChangePage">
         <ul class="records__historic">
           <li v-for="(report, index) in historic"
           class="records__list-item"
@@ -107,7 +106,7 @@ export default {
               :id="report.randomQuiz ? report.randomQuiz.id_quiz : report.customQuiz.id_quiz"
               key="index"
               :class="{selected: selectedMatch === index}"
-              @click="handleSelectedItem(index,
+              @click="handleSelectedMatch(index,
               report.randomQuiz ? report.randomQuiz.id_quiz : report.customQuiz.id_quiz)"
             >
               <p class="records__title" v-if="report.randomQuiz">{{report.randomQuiz.mode}}</p>
@@ -120,9 +119,12 @@ export default {
         </ul>
       </pagination>
     </article>
+
   </aside>
 
+  <!--Report-->
   <section class="records__inform">
+
     <article class="records__report" aria-label="Game report">
       <h1 >Game rep</h1>
       <img class="records__glass"
@@ -135,21 +137,18 @@ export default {
       <div class="records__container">
         <hr class="records__underline">
       </div>
-
     </article>
 
-    <p v-if="!this.selectedMatch" class="records__note">Choose a quizz to see the report :)</p>
+    <p v-if="this.selectedMatch === null" class="records__note">Choose a quizz to see the report :)</p>
 
-    <section class="records__quiz" v-if="selectedMatch !== null">
+    <section class="records__quiz" v-if="this.selectedMatch !== null && this.questionsInfo.length && this.answersInfo.length">
 
       <article class="records__summary">
         <p class="records__minititle">
           Quiz name/mode:
-          {{ (this.historic[selectedMatch].randomQuiz
-          && this.historic[selectedMatch].randomQuiz.mode)
-        ||
-        (this.historic[selectedMatch].customQuiz
-          && this.historic[selectedMatch].customQuiz.quiz_name) }}
+          {{ (this.historic[selectedMatch].randomQuiz && this.historic[selectedMatch].randomQuiz.mode)
+              ||
+            (this.historic[selectedMatch].customQuiz && this.historic[selectedMatch].customQuiz.quiz_name) }}
         </p>
         <p>Total questions: {{ this.historic[selectedMatch].match.quiz.number_questions }}</p>
 
