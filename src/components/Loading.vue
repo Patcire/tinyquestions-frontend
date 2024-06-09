eea74001c988d9b736163ba6a30327e4118594f4
Patricio <pcifrey501@g.educaand.es>
2024-06-09 23:37:35 +0200

    img src

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -10,12 +10,11 @@ export default {
 git
 <template>
   <article class="loading">
-    <!--
-    <img v-if="this.imgSrc"
+    <img v-if="this.imgSrc.includes('clock')"
       class="loading__img"
       :src="imgSrc"
       alt="doodle">
-      -->
+
     <div class="loading__subcontainer">
       <p class="loading__word">{{keyWord}}</p>
       <span class="loading__detail">...</span>
