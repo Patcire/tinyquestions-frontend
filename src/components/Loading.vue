8b2614d1d57b9750db60b1c3f5ff457dab1465ef
Patricio <pcifrey501@g.educaand.es>
2024-06-09 22:58:11 +0200

    svg dont work when build

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -10,10 +10,12 @@ export default {
 git
 <template>
   <article class="loading">
+    <!--
     <img v-if="this.imgSrc"
       class="loading__img"
       :src="imgSrc"
       alt="doodle">
+      -->
     <div class="loading__subcontainer">
       <p class="loading__word">{{keyWord}}</p>
       <span class="loading__detail">...</span>
