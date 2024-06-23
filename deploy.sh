7e2c32c8e95ec7c69db5fc455a35a1ccbc53aa01
Patricio <pcifrey.lab@gmail.com>
2024-06-23 15:31:29 +0200

    cdeploy functional

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-DEPLOY_PATH="/tinyquestions-frontend"
-
 chown -R www-data /root/tinyquestions-frontend/dist/
 systemctl restart apache2
-cd ${DEPLOY_PATH}
+cd /root/tinyquestions-frontend/dist/
