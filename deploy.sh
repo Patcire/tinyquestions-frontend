f6255b63b0bd990126cc224db41d077d37de01c9
Patricio <pcifrey.lab@gmail.com>
2024-06-23 18:11:59 +0200

    cdploy 8

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+# continue deployment
+cd /root/tinyquestions-frontend
+pnpm install
+pnpm build
 chown -R www-data /root/tinyquestions-frontend/dist/
 systemctl restart apache2
-cd /root/tinyquestions-frontend/dist/
