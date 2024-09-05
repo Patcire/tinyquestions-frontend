83f35589b8209e3c941f93a7c92e8f2d8b0345a0
Patricio <pcifrey.lab@gmail.com>
2024-09-05 19:22:25 +0200

    fix display of hambmenu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -56,30 +56,29 @@ export default {
   <!--no connected-->
   <nav class= "hambmenu" v-if="!this.sessionStore.isConnected">
       <article class="hambmenu__icon">
-        <input aria-label="drop menu on mobiles" type="checkbox" class="hambmenu__check">
-          <label for="hambmenu__check">
-            <img src="../../public/hambmenu.svg"
-                 alt="Menu icon"
-                  class="hambmenu__img">
-          </label>
+          <input aria-label="drop menu on mobiles" type="checkbox" class="hambmenu__check">
+            <label for="hambmenu__check">
+              <img src="../../public/hambmenu.svg"
+                   alt="Menu icon"
+                    class="hambmenu__img">
+            </label>
 
-        <ul class="hambmenu__submenu" v-if="!sessionStore.user.isConnected">
-          <li>
-            <router-link to="/play" class="navbar__link">Play</router-link>
-          </li>
-          <li>
-            <router-link to="/profile" class="navbar__link">Community</router-link>
-          </li>
-          <li>
-            <router-link to="/about" class="navbar__link">About</router-link>
-          </li>
-        </ul>
+          <ul class="hambmenu__submenu" v-if="!sessionStore.user.isConnected">
+            <li>
+              <router-link to="/play" class="navbar__link">Play</router-link>
+            </li>
+            <li>
+              <router-link to="/profile" class="navbar__link">Community</router-link>
+            </li>
+            <li>
+              <router-link to="/about" class="navbar__link">About</router-link>
+            </li>
+          </ul>
       </article>
-
   </nav>
 
   <!--connected-->
-  <nav class= "hambmenu hambmenu--mod" v-if="this.sessionStore.user.isConnected">
+  <nav class= "hambmenu" v-if="this.sessionStore.user.isConnected">
       <article class="hambmenu__icon hambmenu__icon--mod">
          <input aria-label="drop menu on mobiles" type="checkbox" class="hambmenu__check">
         <label for="hambmenu__check">
