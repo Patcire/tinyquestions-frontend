0dac969573d95f31c78ba876153b3020ee14a6e2
Patricio <pcifrey.lab@gmail.com>
2024-09-22 22:58:51 +0200

    improves on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,40 +21,29 @@ export default {
 
 <template>
 
-  <!--DESKTOP-->
-  <!--no connected-->
-  <nav>
-    <ul v-if="!this.sessionStore.user.isConnected" class="navbar">
+  <nav class="navbar">
+    <ul>
       <li>
         <router-link to="/play" class="navbar__link">Play</router-link>
       </li>
       <li>
         <router-link to="/profile" class="navbar__link">Community</router-link>
       </li>
-      <li>
+      <li v-if="!this.sessionStore.user.isConnected">
         <router-link to="/about" class="navbar__link">About</router-link>
       </li>
-    </ul>
-    <!--connected-->
-    <ul v-if="this.sessionStore.user.isConnected" class="navbar navbar--mod">
-      <li>
-        <router-link to="/play" class="navbar__link navbar__link--mod">Play</router-link>
+      <li v-if="this.sessionStore.user.isConnected">
+        <router-link to="/create" class="navbar__link" >Create</router-link>
       </li>
-      <li>
-        <router-link to="/create" class="navbar__link navbar__link--mod" >Create</router-link>
-      </li>
-      <li class="navbar__submenu">
-        <router-link to="/profile" class="navbar__link navbar__link--mod" >Comunnity</router-link>
-      </li>
-      <li>
-        <a @click="exit" class="navbar__link pointer navbar__link pointer--mod" >Exit</a>
+      <li v-if="this.sessionStore.user.isConnected">
+        <a @click="exit" class="navbar__link pointer" >Exit</a>
       </li>
     </ul>
   </nav>
 
   <!--TABLET/MOBILE-->
   <!--no connected-->
-  <nav class= "hambmenu" v-if="!this.sessionStore.isConnected">
+  <nav class= "hambmenu">
       <article class="hambmenu__icon">
           <input aria-label="drop menu on mobiles" type="checkbox" class="hambmenu__check">
             <label for="hambmenu__check">
@@ -63,45 +52,25 @@ export default {
                     class="hambmenu__img">
             </label>
 
-          <ul class="hambmenu__submenu" v-if="!sessionStore.user.isConnected">
+          <ul class="hambmenu__submenu">
             <li>
               <router-link to="/play" class="navbar__link">Play</router-link>
             </li>
             <li>
               <router-link to="/profile" class="navbar__link">Community</router-link>
             </li>
-            <li>
+            <li v-if="!this.sessionStore.user.isConnected">
               <router-link to="/about" class="navbar__link">About</router-link>
+            </li>
+            <li v-if="this.sessionStore.user.isConnected">
+              <router-link to="/create" class="navbar__link" >Create</router-link>
+            </li>
+            <li v-if="this.sessionStore.user.isConnected">
+              <a @click="exit" class="navbar__link pointer" >Exit</a>
             </li>
           </ul>
       </article>
   </nav>
 
-  <!--connected-->
-  <nav class= "hambmenu" v-if="this.sessionStore.user.isConnected">
-      <article class="hambmenu__icon hambmenu__icon--mod">
-         <input aria-label="drop menu on mobiles" type="checkbox" class="hambmenu__check">
-        <label for="hambmenu__check">
-          <img src="../../public/hambmenu.svg"
-               alt="Menu icon"
-               class="hambmenu__img">
-        </label>
-        <ul class="hambmenu__submenu hambmenu__submenu--mod">
-          <li>
-            <router-link to="/play" class="navbar__link">Play</router-link>
-          </li>
-          <li>
-            <router-link to="/create" class="navbar__link" >Create</router-link>
-          </li>
-          <li class="navbar__submenu">
-            <router-link to="/profile" class="navbar__link">Comunnity</router-link>
-          </li>
-          <li>
-            <a @click="exit" class="navbar__link pointer" >Exit</a>
-          </li>
-        </ul>
-      </article>
-
-  </nav>
-
 </template>
+
