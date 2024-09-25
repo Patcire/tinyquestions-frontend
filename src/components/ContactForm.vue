81d1276b3df0d6c3ca587fac93548a67f95e679a
Patricio <pcifrey.lab@gmail.com>
2024-09-25 18:18:18 +0200

    wip: modal contact

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -24,30 +24,28 @@ export default {
 
   <vForm :validation-schema="this.schema" class="contact">
 
-          <header class="contact__title">
-            <h3>Tell us your problem</h3>
-          </header>
+    <header class="contact__title">
+      <h3>Tell us your problem</h3>
+    </header>
 
+    <div class="contact__input-container">
       <vField class="contact__input" type="email" name="email" placeholder=" Email..." aria-label="email"/>
       <vError name="email" class="form__global-error"></vError>
+    </div>
 
-      <fieldset class="contact__input contact__input--mod">
-        <label class="contact__label">Date of the incidence
-          <vField type="date" name="date" class="contact__date"/>
-        </label>
-        <vError name="date" class="form__global-error"></vError>
-      </fieldset>
-
-        <vField as="textarea" name="textarea" placeholder="Your message..." aria-label="textarea" class="contact__textarea"></vField>
-      <vError name="textarea" class="form__global-error"></vError>
-
+    <div class="contact__input-container">
       <fieldset class="contact__input contact__input--mod">
         <label class="contact__label">Accept our privacy policy
           <vField type="checkbox" name="policy" :value="true" aria-label="checkbox privacy policy" class="contact__checkbox"/></label>
-        <vError name="policy" class="form__global-error"></vError>
       </fieldset>
+      <vError name="policy" class="form__global-error"></vError>
+    </div>
 
-    <button type="submit" class="contact__button">Send</button>
+    <div class="contact__textarea-container">
+      <vField as="textarea" name="textarea" placeholder="Your message..." aria-label="textarea" class="contact__textarea"></vField>
+      <vError name="textarea" class="form__global-error"></vError>
+    </div>
+
   </vForm>
 
 </template>
