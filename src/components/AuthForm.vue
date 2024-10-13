045e892740e530571ba09b649e59a09fea67ae1e
Patricio <pcifrey.lab@gmail.com>
2024-10-13 17:15:16 +0200

    try change broke img and fix router link

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -40,7 +40,7 @@ export default {
         return
       }
       this.isLoading = false
-      check && await router.push('/games')
+      check && await router.push('/play')
     },
 
     async handleRegister(){
